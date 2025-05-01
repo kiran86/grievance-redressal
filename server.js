4bb105a189422d9fb7b753af3ccaf5160b22482f
Kiran Sankar Das <kiran.sankar.das@gmail.com>
2025-05-01 21:17:52 +0530

    feat: add dotenv for environment variable management and implement complaint submission form

--- server.js
+++ server.js
@@ -1,2 +1,41 @@
+require('dotenv').config();
 const express = require('express');
-const bodyPerser = require('body-parser')
+const bodyPerser = require('body-parser');
+const mysql = require('mysql2/promise')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const req = require('express/lib/request');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+// Database connection pool
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// Middleware to verify JWT token
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, JWT_SECRET_KEY, (err, user) => {
+            if (err) {
+                return res.sendStatus(403); // Forbidden
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401); // Unauthorized
+    }
+};
